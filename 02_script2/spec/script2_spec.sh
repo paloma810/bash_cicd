3b8de238792708325aa3c3d27f7699e2031f056b
paloma810 <mcyk49129@gmail.com>
2024-07-26 16:42:43 +0900

    ci test

--- 02_script2/spec/script2_spec.sh
+++ 02_script2/spec/script2_spec.sh
@@ -1,6 +1,6 @@
 Describe "script1.sh"
   Example "says hello"
     When call ./script2.sh
-    The output should equal "Hello from script1"
+    The output should equal "Hello from script2"
   End
 End
